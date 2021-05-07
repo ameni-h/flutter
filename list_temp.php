8141197288543b6088bf4c49d7385a19f64f91a5
ameni-h <81304547+ameni-h@users.noreply.github.com>
2021-05-07 23:58:12 +0100

    Create list_temp.php

---
+++ list_temp.php
@@ -0,0 +1,15 @@
+<?php
+    include "db.php";
+    $query=$db->prepare('select * from dht11');
+    $query->execute();
+    if ($query->rowCount()>0) {
+    	$data=$query->fetchAll(PDO::FETCH_ASSOC);
+    	echo json_encode($data);
+    }
+    /*else{
+    	$json['success']=0;
+    	$json['myintro']='';
+    	$json['message']='no data found';
+    	echo json_encode($json);
+    }*/
+    ?>
